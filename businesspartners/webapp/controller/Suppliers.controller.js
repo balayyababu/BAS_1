72f460bf2c5942839b3b3c814cae06fc79e0bc32
Balakrishna.V <35360980+balayyababu@users.noreply.github.com>
2022-04-14 07:49:25 +0000

    My first Commit and Push

---
+++ businesspartners/webapp/controller/Suppliers.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, MessageToast) {
+        "use strict";
+
+        return Controller.extend("ns.businesspartners.controller.Suppliers", {
+            onInit: function () {
+
+            },
+            _Save: function () {
+                MessageToast.show("Save Button Pressed", { at: sap.ui.core.Popup.Dock.CenterTop });
+            },
+            _Change: function () {
+                MessageToast.show("Chnage Button Pressed", { at: sap.ui.core.Popup.Dock.RightBottom });
+            },
+            _Display: function () {
+                MessageToast.show("Dislay Button Pressed", { at: sap.ui.core.Popup.Dock.LeftBottom });
+            }
+        });
+    });
